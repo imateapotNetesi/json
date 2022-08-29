4a1490140e7b918c776469696b5b5078427106b8
imateapotNetesi <111732869+imateapotNetesi@users.noreply.github.com>
2022-08-29 17:24:33 +0200

    missing

--- src/Middleware.php
+++ src/Middleware.php
@@ -14,15 +14,15 @@ class Middleware implements MiddlewareInterface {
       try {
         $response = $handler->handle($request);
       } catch (Exception $e) {
-        new Payload(null,$e->getMessage(),$e->getCode());
+
+        $payload = new Payload(null,$e->getMessage(),$e->getCode());
 
         $code = $e->getCode();
-
         if (  !($e->getCode() > 400 && $e->getCode() < 599) ) $code = 400;
 
         $psr17Factory = new Psr17Factory();
 
-        $responseBody = $psr17Factory->createStream( json_encode((object) $payload, JSON_PRETTY_PRINT) );
+        $responseBody = $psr17Factory->createStream( (string) $payload );
         $response = $psr17Factory->createResponse($code)->withBody($responseBody);
       }
       return $response->withHeader('Content-type', 'application/json');
